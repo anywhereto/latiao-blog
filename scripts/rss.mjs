6aabcccdcb2f317d1e1efcd284a6b363b0a1cb1d
pekingzcc <pekingzcc@gmail.com>
2025-02-08 12:13:32 +0800

    bug fix: node version compatiable issue

--- scripts/rss.mjs
+++ scripts/rss.mjs
@@ -3,9 +3,12 @@ import path from 'path'
 import { slug } from 'github-slugger'
 import { escape } from 'pliny/utils/htmlEscaper.js'
 import siteMetadata from '../data/siteMetadata.js'
-import tagData from '../app/tag-data.json' assert { type: 'json' }
 import { allBlogs } from '../.contentlayer/generated/index.mjs'
 import { sortPosts } from 'pliny/utils/contentlayer.js'
+
+// 使用 JSON.parse 和 fs 来读取 JSON 文件
+import { readFileSync } from 'fs'
+const tagData = JSON.parse(readFileSync(new URL('../app/tag-data.json', import.meta.url)))
 
 const outputFolder = process.env.EXPORT ? 'out' : 'public'
 
