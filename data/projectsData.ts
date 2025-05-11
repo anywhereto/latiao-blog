0b2b68756f5f778c12cf4a7160d09b6afda3a241
george <pekingzcc@gmail.com>
2025-05-11 11:45:06 +0800

    remove sth

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -14,42 +14,10 @@ const projectsData: Project[] = [
     href: 'https://www.dev-life.site/',
   },
   {
-    title: 'Free Things To Do',
-    description: `Find the best free things to do in your area. Perfect for families, students, 
-    and anyone looking for cost-free entertainment..`,
-    imgSrc: '/static/images/free-things-to-do.png',
-    href: 'https://freethingstodo.net/',
-  },
-  {
-    title: 'Street Racer Game',
-    description: `A game studio collected street racer games from all over the world.`,
-    imgSrc: '/static/images/street-racer.png',
-    href: 'https://streetracer.online/',
-  },
-  {
-    title: 'How to explain',
-    description: `An AI tool to help you explain things to others.`,
-    imgSrc: '/static/images/how_to_explain.png',
-    href: 'https://howtoexplain.xyz/',
-  },
-  {
     title: 'Nova AI Toolkit',
     description: `A comprehensive platform integrating various AI tools.`,
     imgSrc: '/static/images/nova.png',
     href: 'https://novatoolkit.ai/',
-  },
-  {
-    title: 'Ghibli Style Image',
-    description: `Transform Your Ideas into Ghibli Style Image.`,
-    imgSrc: '/static/images/ghibli.png',
-    href: 'https://ghiblistyleimage.org/',
-  },
-  {
-    title: 'dreamactor-m1',
-    description: `Holistic, Expressive and Robust Human Image Animation
-with Hybrid Guidance`,
-    imgSrc: '/static/images/dreamactor.png',
-    href: 'https://dreamactor-m1.org/',
   },
   {
     title: 'Query GPT',
